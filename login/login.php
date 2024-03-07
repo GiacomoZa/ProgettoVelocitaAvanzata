75d0d47f37bcb7adbf8c74da6028b56c1dbd65ca
samuele-05 <8293469@itisrossi.vi.it>
2024-03-07 19:32:03 +0100

    login-funzionante-meta

--- login/login.php
+++ login/login.php
@@ -1,41 +1,48 @@
 <?php
+session_start();
+
 $hostname = "localhost";
 $username = "root";
 $password = "";
 $dbname = "dbvelocitaavanzata";
 
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
+
 // Check connection
-if ($conn) {
-    echo "Connessione al database riuscita";
-} else {
+if (!$conn) {
     die("Errore nella connessione: " . mysqli_connect_error());
 }
 
-$user = $_POST['user'];
-$psw = $_POST['psw'];
-if ($user == " || $psw == "){
-    print "Campi vuoti";
-    print "<br><a href='group-1.html'>Torna al login</a>";
-}
-else{
-    $query = "SELECT * FROM Utente WHERE user = '$user' && password = '$psw'";
-    $risultato= mysqli_query($conn, $query);
-    if (!$risultato){
-        print "errore nel comando"; 
-        exit();
-    }
+// Verifica se è stato inviato un modulo con il metodo POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Se il modulo è stato inviato, esegui la logica di autenticazione
+    $user = $_POST['user'];
+    $psw = $_POST['psw'];
 
-    $riga = mysqli_fetch_array($risultato);
-    
-    if ($riga)
-        print "Benvenuto ". $riga['nome'].' '. $riga['cognome'];
-    else{
-        print "username o password errate";
-        print "<br><a href='group-1.html'>torna al login</a>"; 
-        print "<br><a href='group-2.html'>registrati</a>";
+    if ($user == "" || $psw == "") {
+        echo "Campi vuoti";
+    } else {
+        $query = "SELECT * FROM Utente WHERE user = '$user' && password = '$psw'";
+        $risultato = mysqli_query($conn, $query);
+        if (!$risultato) {
+            echo "Errore nel comando"; 
+            exit();
+        }
+
+        $riga = mysqli_fetch_array($risultato);
+
+        if ($riga) {
+            // Imposta la variabile di sessione per l'utente
+            $_SESSION['username'] = $user;
+            // Restituisci l'username dell'utente come risposta alla richiesta AJAX
+            echo $user;
+        } else {
+            echo "Username o password errate";
+        }
     }
 }
 
 mysqli_close($conn);
+
+
 ?>
